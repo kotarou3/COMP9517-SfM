c7e8f5040ee54ee733962cd4a87feb2ea312077b
小太 <nospam@kota.moe>
2018-05-12 21:04:48 +1000

    Remove trailing spaces

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ import tempfile
 
 def calibrateCamera(imagePaths, shape):
     """Calculates intrinsic camera matrix from a chessboard pattern.
-    
+
     imagePaths = List of input images
     shape = (columns, rows) of the chessboard
-    
+
     Assumes the images are already corrected for lens distortion.
-    
+
     Each square of the chessboard is treated as a unit square in the resulting
     world coordinates. e.g., If you use 1x1cm squares, then the units of the
     world coordinates are in cm.
@@ -25,7 +25,7 @@ def calibrateCamera(imagePaths, shape):
     # Define the chessboard's world points
     chessboardWPoints = np.zeros((corners[0] * corners[1], 3), np.float32)
     chessboardWPoints[:, :2] = np.mgrid[0:corners[0], 0:corners[1]].T.reshape(-1, 2)
-    
+
     # Arrays to store object points and image points from all the images.
     worldPoints = [] # 3D points in world space
     imagePoints = [] # 2D points in image plane
@@ -37,24 +37,24 @@ def calibrateCamera(imagePaths, shape):
             imageShape = grey.shape
         else:
             assert(imageShape == grey.shape)
-        
+
         # Find the chess board corners
         found, foundCorners = cv2.findChessboardCorners(
             grey, corners,
             flags = cv2.CALIB_CB_ADAPTIVE_THRESH | cv2.CALIB_CB_NORMALIZE_IMAGE
         )
-        
+
         # If found, add object points, image points (after refining them)
         if found:
             worldPoints.append(chessboardWPoints)
             imagePoints.append(foundCorners)
-            
+
             # Draw and display the corners
             """cv2.drawChessboardCorners(image, corners, foundCorners, found)
             cv2.imshow("Detected Chessboards", image)
             cv2.waitKey(500)
     cv2.destroyAllWindows()"""
-    
+
     reprojError, cameraMatrix, _, _, _ = cv2.calibrateCamera(
         worldPoints, imagePoints, imageShape, None, None,
         flags = cv2.CALIB_FIX_ASPECT_RATIO |
@@ -63,7 +63,7 @@ def calibrateCamera(imagePaths, shape):
             cv2.CALIB_FIX_K1 | cv2.CALIB_FIX_K2 | cv2.CALIB_FIX_K3
     )
     assert(reprojError < 0.5)
-    
+
     return cameraMatrix
 
 def detectFeatures(image):
@@ -100,32 +100,32 @@ def matchFeatures(descriptorsA, descriptorsB):
 
 def trackFeatures(images):
     assert(len(images) >= 2)
-    
+
     # Detect the features
     keypoints, descriptors = list(zip(*(detectFeatures(image) for image in images)))
-    
+
     # Match the features along every adjacent pair of images
     matches = []
     for i in range(len(images) - 1):
         descriptorsA = descriptors[i]
         descriptorsB = descriptors[i + 1]
         matches.append(matchFeatures(descriptorsA, descriptorsB))
-    
+
     return keypoints, matches
 
 def findProjectionMatrices(keypoints, matches, intrinsicMatrix):
     """Finds the projection matrix for each camera relative to the first camera.
-    
+
     keypoints = [keypoints of each image]
     matches = matches between keypoints[i] and keypoints[i + 1]
     intrinsicMatrix = Shared intrinsic camera matrix of all the cameras
-    
+
     Updates the matches to only include inliers, and returns the 4x3 projection
     matrices.
     """
 
     assert(len(keypoints) == len(matches) + 1)
-    
+
     matrices = []
     prevRotation = np.identity(3)
     prevTranslation = np.zeros((3, 1))
@@ -135,29 +135,29 @@ def findProjectionMatrices(keypoints, matches, intrinsicMatrix):
         pointsA = np.array([keypoints[i][m.queryIdx].pt for m in match])
         pointsB = np.array([keypoints[i + 1][m.trainIdx].pt for m in match])
         essentialMatrix, mask = cv2.findEssentialMat(pointsA, pointsB, intrinsicMatrix)
-        
+
         # If #inliers < 50% of matching points, probably wasn't a good matrix
         #print(mask)
         #assert(np.count_nonzero(mask) >= 0.5 * len(match))
-        
+
         # Recover the relative rotation and translation of the cameras.
         # recoverPose() gives the relative transformation of camera B from A.
         _, rotation, translation, _ = cv2.recoverPose(
             essentialMatrix, pointsA, pointsB,
             intrinsicMatrix, mask = mask
         )
-        
+
         # Invert the transforms so we get the transformation from camera A to B
         rotation = np.linalg.inv(rotation)
         translation = -translation
-        
+
         # Update the matches to only include inliers
         inlierMatches = []
         for idx, m in enumerate(match):
             if mask[idx][0]:
                 inlierMatches.append(m)
         matches[i] = inlierMatches
-        
+
         # Compute the projection matrix
         prevRotation = rotation @ prevRotation
         prevTranslation = translation + prevTranslation
